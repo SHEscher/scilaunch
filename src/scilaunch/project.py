529aa24ae3690600c8d48ece2d785e03341a18cd
simon.m.hofmann <simon.hofmann@cbs.mpg.de>
2023-11-30 16:23:26 +0100

    add check whether boolean default vars are set correctly

--- src/scilaunch/project.py
+++ src/scilaunch/project.py
@@ -4,6 +4,7 @@
 import json
 from pathlib import Path
 
+import yaml
 from cookiecutter.main import cookiecutter
 from git import Repo
 from git.exc import GitCommandError
@@ -16,6 +17,77 @@ COOKIECUTTERRC = Path.home() / ".cookiecutterrc"  # destination for cookiecutter
 
 
 # %% Functions >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
+
+
+def _check_str_to_bool(val: str):
+    """
+    Check whether the given string can be converted to a boolean.
+
+    :param str val: String to be checked.
+    :return: True if the string can be converted to a boolean, False otherwise.
+    :rtype: bool
+    """
+    if val.lower() in ["n", "no", "false", "f", "0", "off"]:
+        return False
+    if val.lower() in ["y", "yes", "true", "t", "1", "on"]:
+        return True
+    return None
+
+
+def _check_int_to_bool(val: int):
+    """
+    Check whether the given integer can be converted to a boolean.
+
+    :param int val: Integer to be checked.
+    :return: True if the integer can be converted to a boolean, False otherwise.
+    :rtype: bool
+    """
+    if val == 0:
+        return False
+    if val == 1:
+        return True
+    return None
+
+
+def check_booleans_in_cookiecutterrc():
+    """Check the validity of the `.cookiecutterrc` file."""
+    # Read .cookiecutterrc file (YAML)
+    with COOKIECUTTERRC.open() as f:
+        cookiecutterrc = yaml.safe_load(f)
+
+    # Read cookiecutter.json file (JSON)
+    with Path(path_to.templates.local.cookiecutterrc, "cookiecutter.json").open() as f:
+        cookiecutter_json = json.load(f)
+
+    # Check whether the cookiecutterrc file has the correct boolean types
+    updated = False
+    for key, default_val in cookiecutter_json.items():
+        if isinstance(default_val, bool):
+            crc_val = cookiecutterrc["default_context"].get(key)
+            if isinstance(crc_val, bool):
+                continue
+
+            # Overwrite "n" -> False and "y" -> True
+            if isinstance(crc_val, str):
+                crc_val = _check_str_to_bool(crc_val)
+            if isinstance(crc_val, int):
+                crc_val = _check_int_to_bool(crc_val)
+            if not isinstance(crc_val, bool):
+                msg = (
+                    f"Invalid type for '{key}': {crc_val} of type {type(crc_val)}."
+                    f"\nPlease check '{COOKIECUTTERRC}' and fill with valid boolean value."
+                )
+                raise TypeError(msg)
+
+            # Set updated boolean default value
+            cookiecutterrc["default_context"][key] = default_val if crc_val is None else crc_val
+            updated = True
+
+    # Write updated .cookiecutterrc file (YAML)
+    if updated:
+        print(f"\033[33m\nUpdating {COOKIECUTTERRC} boolean default values ...\033[0m")
+        with COOKIECUTTERRC.open("w") as f:
+            yaml.dump(cookiecutterrc, f, sort_keys=False, indent=4)
 
 
 def create_cookiecutterrc(**kwargs):
@@ -62,6 +134,7 @@ def create_cookiecutterrc(**kwargs):
         )
 
     else:
+        check_booleans_in_cookiecutterrc()
         print(f"\nUsing existing {COOKIECUTTERRC} to fill defaults.\n")
 
 
@@ -79,10 +152,10 @@ def is_git_repo_up_to_date(path):
     try:
         upstream.fetch(kill_after_timeout=10)  # run git fetch to update the remote-tracking branches
     except GitCommandError as e:
-        additionaL_err_msg = (
+        additional_err_msg = (
             "\033[31mUnable to fetch data from remote repository! Check your internet connection!\033[0m"
         )
-        raise GitCommandError(str(e) + additionaL_err_msg) from e
+        raise GitCommandError(str(e) + additional_err_msg) from e
     return repo.head.commit == upstream.refs.main.commit
 
 
--- src/tests/test_scilaunch.py
+++ src/tests/test_scilaunch.py
@@ -6,18 +6,77 @@ Run with:
     pytest --cov --cov-report=html
 
 """
-
 # %% Import
 import shutil
 from pathlib import Path
 
-# import pytest
+import pytest
 import toml
+import yaml
 from scilaunch import configs, project
 
 # %% Set global vars & paths >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
 
-SCILAUNCH_CACHE = Path.home() / ".cache/scilaunch"
+
+@pytest.fixture()
+def temp_scilaunch_cache():
+    """Create temp cache dir."""
+    scilaunch_cache = Path.home() / ".cache/scilaunch"
+    test_project_name = "testproject"
+
+    yield scilaunch_cache, test_project_name
+
+    # Tear down
+    # Remove the test project
+    if (scilaunch_cache / test_project_name).exists():
+        shutil.rmtree(scilaunch_cache / test_project_name)
+
+    # Check if cache dir is empty, if so remove it
+    if not list(scilaunch_cache.iterdir()):
+        scilaunch_cache.rmdir()
+
+
+@pytest.fixture()
+def temp_cookiecutterrc():
+    """Create temporary ~/.cookiecutterrc file."""
+    if project.COOKIECUTTERRC.exists():
+        # Make backup
+        project.COOKIECUTTERRC.rename(project.COOKIECUTTERRC.with_suffix(".bak"))
+
+    # Create dir
+    project.COOKIECUTTERRC.parent.mkdir(parents=True, exist_ok=True)
+    cookiecutterrc = {
+        "default_context": {
+            "full_name": "Test Author",
+            "email": "pytest@author.me",
+            "github_username": "PyTester",
+            "create_conda_env": "y",
+            "init_git": "no",
+            "use_pytest": 1,
+            "use_black": False,
+            "linting": "ruff",
+        },
+        "abbreviations": {
+            "gh": "https://github.com/{0}.git",
+            "rp": "https://github.com/SHEscher/research-project.git",
+        },
+    }
+    # Write temporary file
+    with project.COOKIECUTTERRC.open("w") as f:
+        yaml.dump(cookiecutterrc, f)
+
+    # Read updated file
+    with project.COOKIECUTTERRC.open() as f:
+        cookiecutterrc = yaml.safe_load(f)
+
+    yield cookiecutterrc
+
+    # Tear down
+    if project.COOKIECUTTERRC.exists():
+        project.COOKIECUTTERRC.unlink()
+    if project.COOKIECUTTERRC.with_suffix(".bak").exists():
+        # Restore backup
+        project.COOKIECUTTERRC.with_suffix(".bak").rename(project.COOKIECUTTERRC)
 
 
 # %% Test Functions o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
@@ -30,23 +89,81 @@ def test_configs():
 
 
 # scilaunch.project
-def test_create_cookiecutterrc():
+def test__check_str_to_bool():
+    """Test _check_str_to_bool()."""
+    for val in ["n", "No", "False", "f", "0", "off"]:
+        assert not project._check_str_to_bool(val)
+
+    for val in ["y", "Yes", "true", "T", "1", "on"]:
+        assert project._check_str_to_bool(val)
+
+    for val in ["a", "b0", "D", "TR", "fA", "g"]:
+        assert project._check_str_to_bool(val) is None
+
+
+def test__check_int_to_bool():
+    """Test _check_int_to_bool()."""
+    for val in [0, 0.0]:
+        assert not project._check_int_to_bool(val)
+    for val in [1, 1.0]:
+        assert project._check_int_to_bool(val)
+    for val in [2, 2.0, -1, -1.0]:
+        assert project._check_int_to_bool(val) is None
+
+
+def test_check_booleans_in_cookiecutterrc(capsys, temp_cookiecutterrc):
+    """Test check_booleans_in_cookiecutterrc()."""
+    # Create temporary cookiecutterrc file
+    _ = temp_cookiecutterrc
+
+    # Update boolean values
+    project.check_booleans_in_cookiecutterrc()
+    out, _ = capsys.readouterr()  # _ = err
+
+    # Read updated file
+    with project.COOKIECUTTERRC.open() as f:
+        cookiecutterrc = yaml.safe_load(f)
+
+    # Check if boolean values are updated
+    assert cookiecutterrc["default_context"]["create_conda_env"] is True
+    assert cookiecutterrc["default_context"]["init_git"] is False
+    assert cookiecutterrc["default_context"]["use_pytest"] is True
+    assert cookiecutterrc["default_context"]["use_black"] is False
+    assert ".cookiecutterrc boolean default values ..." in out
+
+    # Check in valid case
+    cookiecutterrc["default_context"]["create_conda_env"] = "invalid"
+    with project.COOKIECUTTERRC.open("w") as f:
+        yaml.dump(cookiecutterrc, f)
+
+    with pytest.raises(TypeError, match="Invalid type for 'create_conda_env':"):
+        project.check_booleans_in_cookiecutterrc()
+
+
+def test_create_cookiecutterrc(capsys, temp_cookiecutterrc):
     """Test create_cookiecutterrc()."""
-    if project.COOKIECUTTERRC.exists():
-        # Make backup
-        project.COOKIECUTTERRC.rename(project.COOKIECUTTERRC.with_suffix(".bak"))
+    # Create temporary cookiecutterrc file
+    _ = temp_cookiecutterrc
+
+    # Test case when .cookiecutterrc file is present
+    project.create_cookiecutterrc()
+    out, _ = capsys.readouterr()  # _ = err
+    assert f"Using existing {project.COOKIECUTTERRC} to fill defaults." in out
+
+    # Test case when no file is present
+    # Remove .cookiecutterrc file temporarily
+    project.COOKIECUTTERRC.unlink()  # if there was a file before, it will be restored in teardown
+
     project.create_cookiecutterrc(no_input=True)
     assert project.COOKIECUTTERRC.exists()
-    if project.COOKIECUTTERRC.with_suffix(".bak").exists():
-        # Restore backup
-        project.COOKIECUTTERRC.with_suffix(".bak").rename(project.COOKIECUTTERRC)
 
 
-def test_create():
+def test_create(temp_scilaunch_cache):
     """Test create()."""
-    test_project_name = "testproject"
+    scilaunch_cache, test_project_name = temp_scilaunch_cache
+
     project.create(
-        out_dir=SCILAUNCH_CACHE,
+        out_dir=scilaunch_cache,
         create_cc_rc=False,
         no_input=True,
         extra_context={
@@ -58,16 +175,8 @@ def test_create():
             "init_git": False,
         },
     )
-    assert (SCILAUNCH_CACHE / test_project_name).exists()
-    assert toml.load(SCILAUNCH_CACHE / test_project_name / "pyproject.toml")["project"]["name"] == test_project_name
-
-    # Remove test project
-    if (SCILAUNCH_CACHE / test_project_name).exists():
-        shutil.rmtree(SCILAUNCH_CACHE / test_project_name)
-
-    # Check if cache dir is empty, if so remove it
-    if not list(SCILAUNCH_CACHE.iterdir()):
-        SCILAUNCH_CACHE.rmdir()
+    assert (scilaunch_cache / test_project_name).exists()
+    assert toml.load(scilaunch_cache / test_project_name / "pyproject.toml")["project"]["name"] == test_project_name
 
 
 # o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o END
